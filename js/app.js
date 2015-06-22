09c6920f463ff6bac004be01bb47a56cb1f3db49
Shipra Sinha <shipra.juet@gmail.com>
2015-06-22 12:49:17 +0000

    Fixed bugs in the header

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function()    {
+$(function(){
+  $("#nav a").click(function(e){
+    e.preventDefault();
+    $('html,body').scrollTo(this.hash,this.hash); 
+  });
+});
+
+ $('.carousel').carousel({
+        interval: 300
+      });
+});
