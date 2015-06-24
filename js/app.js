1d31ae20f0bc567715f4ab3db7b2e6c82f2aaa36
Shipra Sinha <shipra.juet@gmail.com>
2015-06-24 11:47:40 +0000

    Added portfolio filter

--- js/app.js
+++ js/app.js
@@ -1,15 +1,30 @@
 $(document).ready(function()    {
- $(function(){
-  $("#nav a").click(function(e){
-    e.preventDefault();
-    $('html,body').scrollTo(this.hash,this.hash); 
+  $(function(){
+    $("#nav a").click(function(e){
+      e.preventDefault();
+      $('html,body').scrollTo(this.hash,this.hash); 
+    });
   });
-});
 
- $('.carousel').carousel({
-        interval: 300
-      });
+  $('.carousel').carousel(function(){
+    interval: 300
+  });
 
-$('body').scrollspy({ target : "#navbar-example"});
+  $('body').scrollspy({ target : "#navbar-example"});
+  
+  $('#portfolio_i a').click(function(ev) {
+    ev.preventDefault();
+    $('#portfolio_i .singolo-thumbnail').addClass('fadeOut');
+    $('#portfolio_i .singolo-thumbnail' + this.dataset.filter).removeClass('fadeOut');
+  });
   
 });
+
+
+
+
+
+
+ 
+   
+
